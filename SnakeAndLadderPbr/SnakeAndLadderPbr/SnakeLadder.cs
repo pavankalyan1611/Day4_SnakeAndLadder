4e3aa072c50e337d94042591aedefe6371bd1a39
Pavankalyan1611 <pkkoppisetti@gmail.com>
2022-07-17 15:19:02 +0530

    [pavan] Player get the Winnig Position100

--- SnakeAndLadderPbr/SnakeAndLadderPbr/SnakeLadder.cs
+++ SnakeAndLadderPbr/SnakeAndLadderPbr/SnakeLadder.cs
@@ -16,32 +16,34 @@ namespace SnakeAndLadderPbr
         public void Game()
         {
             int Position = 0;
-            Console.WriteLine($"Initial Position Of the Player Is \"{Position}\"");
-
-            Random random = new Random();
-            int Die = random.Next(1, 7);
-            Console.WriteLine($"The number got by dice roll is {Die}");
-            Random random2 = new Random();
-            int option = random2.Next(0, 3);
-            switch (option)
+            Console.WriteLine($"Initial Position Of the Player Is \"{Position}\"\n");
+            for (; Position <= 100;)
             {
-                case LADDER:
-                    Position += Die;
-                    Console.WriteLine("The player climbed the ladder");
-                    break;
-                case SNAKE:
-                    Position -= Die;
-                    if (Position < 0)
-                    {
-                        Console.WriteLine("The player bit by snake");
-                        Position = 0;
-                    }
-                    else                    
-                        Console.WriteLine("The player bit by snake");                    
-                    break;
-                default:
-                    Console.WriteLine($"No play");
-                    break;
+                Random random = new Random();
+                int Die = random.Next(1, 7);
+                Console.WriteLine($"The number got by dice roll is {Die}");
+                Random random2 = new Random();
+                int option = random2.Next(0, 3);
+                switch (option)
+                {
+                    case LADDER:
+                        Position += Die;
+                        Console.WriteLine("The player climbed the ladder");
+                        break;
+                    case SNAKE:
+                        Position -= Die;
+                        if (Position < 0)
+                        {
+                            Console.WriteLine("The player bit by snake");
+                            Position = 0;
+                        }
+                        else
+                            Console.WriteLine("The player bit by snake");
+                        break;
+                    default:
+                        Console.WriteLine($"No play");
+                        break;
+                }
             }
             Console.WriteLine($"The player position is {Position} \n");
         }
