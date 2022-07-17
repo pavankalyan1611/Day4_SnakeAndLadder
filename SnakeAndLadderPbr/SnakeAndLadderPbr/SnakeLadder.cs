b7904615d8c5ce28d9ca4703a7d78ac0cdf1245b
Pavankalyan1611 <pkkoppisetti@gmail.com>
2022-07-17 15:08:01 +0530

    [pavan] Check getting Ladder, Snake or Noplay

--- SnakeAndLadderPbr/SnakeAndLadderPbr/SnakeLadder.cs
+++ SnakeAndLadderPbr/SnakeAndLadderPbr/SnakeLadder.cs
@@ -8,9 +8,10 @@ namespace SnakeAndLadderPbr
 {
     public class SnakeLadder
     {
+        public const int LADDER = 1, SNAKE = 2;
         public SnakeLadder()
         {
-            Console.WriteLine("Welcome To snake and Ladder Game :");
+            Console.WriteLine("Welcome To snake and Ladder Game :\n");
         }
         public void Game()
         {
@@ -20,7 +21,29 @@ namespace SnakeAndLadderPbr
             Random random = new Random();
             int Die = random.Next(1, 7);
             Console.WriteLine($"The number got by dice roll is {Die}");
-
+            Random random2 = new Random();
+            int option = random2.Next(0, 3);
+            switch (option)
+            {
+                case LADDER:
+                    Position += Die;
+                    Console.WriteLine("The player climbed the ladder");
+                    break;
+                case SNAKE:
+                    Position -= Die;
+                    if (Position < 0)
+                    {
+                        Console.WriteLine("The player bit by snake");
+                        Position = 0;
+                    }
+                    else                    
+                        Console.WriteLine("The player bit by snake");                    
+                    break;
+                default:
+                    Console.WriteLine($"No play");
+                    break;
+            }
+            Console.WriteLine($"The player position is {Position} \n");
         }
     }
 }
